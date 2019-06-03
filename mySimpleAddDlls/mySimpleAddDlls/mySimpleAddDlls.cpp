9ac6cd80ee93f08253b6bb9036c8c3eb0e6428a5
Li Jixiang <jixiang.li@razer.com>
2019-06-03 16:42:55 +0800

    Add LoadTime import dll method sample client app.

---
+++ mySimpleAddDlls/mySimpleAddDlls/mySimpleAddDlls.cpp
@@ -0,0 +1,13 @@
+// mySimpleAddDlls.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+#include "mySimpleAddDlls.h"
+
+#include <utility>
+#include <limits.h>
+
+int simpleAdd(int a, int b) {
+	return a + b;
+}
+
---
+++ mySimpleAddDlls/mySimpleAddDlls/mySimpleAddDlls.h
@@ -0,0 +1,12 @@
+#pragma once
+// MathLibrary.h - Contains declarations of math functions
+#pragma once
+
+#ifdef MATHLIBRARY_EXPORTS
+#define MATHLIBRARY_API __declspec(dllexport)
+#else
+#define MATHLIBRARY_API __declspec(dllimport)
+#endif
+
+
+extern "C" MATHLIBRARY_API int simpleAdd(int a, int b);
